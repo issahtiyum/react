18382103b03c55cd89e056ad3d83d5ff36ecf11f
Haris Issah <myharis.issah@gmail.com>
2024-12-27 18:49:33 +0200

    Remove unnecessary whitespace

--- src/components/PricingCard/PricingCard.jsx
+++ src/components/PricingCard/PricingCard.jsx
@@ -18,65 +18,34 @@ export default function PricingCard({
   const themeClass = themeClasses[label]
 
   return (
-    <div
-      className={`${styles.card} ${themeClass}`}
-    >
-      <div
-        className={`${styles.card__wrapper} center-vertical`}
-      >
+    <div className={`${styles.card} ${themeClass}`}>
+      <div className={`${styles.card__wrapper} center-vertical`}>
         <span className={styles.card__label}>
           {label}
         </span>
         <div className={styles.card__image}>
-          <img src={image} alt={imageAlt} />
+          <img src={image} alt={imageAlt}/>
         </div>
-        <div
-          className={styles["card__price-label"]}
-        >
-          <span
-            className={
-              styles["card__price-figure"]
-            }
-          >
+        <div className={styles["card__price-label"]}>
+          <span className={styles["card__price-figure"]}>
             {price}
           </span>
-          <span
-            className={
-              styles["card__price-duration"]
-            }
-          >
+          <span className={styles["card__price-duration"]}>
             {duration}
           </span>
         </div>
-        <span
-          className={
-            styles["card__benefits-label"]
-          }
-        >
+        <span className={styles["card__benefits-label"]}>
           Everything in Free, Plus
         </span>
         <ul className={styles.card__benefits}>
-          {benefits.map(benefit => {
-            return (
-              <li
-                key={benefit}
-                className={`${styles.card__benefit} center-horizontal`}
-              >
-                <img
-                  src="/icons/check.png"
-                  alt=""
-                  className={
-                    styles["card__benefit-icon"]
-                  }
-                />
+          {benefits.map(benefit => { return (
+              <li key={benefit} className={`${styles.card__benefit} center-horizontal`} >
+                <img src="/icons/check.png" alt="" className={styles["card__benefit-icon"] }/>
                 {benefit}
               </li>
-            )
-          })}
+            )})}
         </ul>
-        <Button className={styles.card__button}>
-          Choose
-        </Button>
+        <Button className={styles.card__button}>Choose</Button>
       </div>
     </div>
   )
